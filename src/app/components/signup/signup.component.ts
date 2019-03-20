e66a7a41e2ce6040a710131a9217f67dd8c123f1
GabrielQuesadaAraya <gabrielquesadajgqa@gmail.com>
2019-03-20 15:23:16 -0600

    Registro y  login en funcionamiento

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { JarwisService } from 'src/app/services/jarwis.service';
+import { TokenService } from 'src/app/services/token.service';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  public form={
+    email:null,
+    name:null,
+    password:null,
+    password_confirmation:null
+  }
+  public error = [];
+  constructor(
+    
+    private Jarwis:JarwisService,
+    private Token : TokenService,
+    private Auth : AuthService,
+    private router: Router,
+
+
+    ) { }
+
+  onSubmit() {
+   this.Jarwis.signup(this.form).subscribe(
+      data => this.handleResponse,
+      error => this.handleError(error)
+    );
+  }
+
+  handleResponse(data) {
+    this.Token.handle(data.access_token);
+    this.Auth.changeAuthStatus(true);
+    this.router.navigateByUrl('profile');
+  }
+  handleError(error){
+    this.error = error.error.errors;
+  }
+  ngOnInit() {
+  }
+
+}
