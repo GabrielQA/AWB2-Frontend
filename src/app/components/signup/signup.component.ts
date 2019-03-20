f36e5d29fc76400df38221671b240ba76d744102
GabrielQuesadaAraya <gabrielquesadajgqa@gmail.com>
2019-03-19 20:16:47 -0600

    Registro validacion correo y campos

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -9,25 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  /*public form={
+  public form={
     email:null,
     name:null,
     password:null,
     password_confirmation:null
-  }*/
-  //public error = [];
-  constructor(/*private http: HttpClient*/) { }
+  }
+  public error = [];
+  constructor(private http: HttpClient) { }
 
-  /*onSubmit(){
+  onSubmit(){
     return this.http.post('http://localhost:8000/api/signup', this.form).subscribe(
       data => console.log(data),
       error => this.handleError(error)
       );   
-  }*/
+  }
 
-  /*handleError(error){
+  handleError(error){
     this.error = error.error.errors;
-  }*/
+  }
   ngOnInit() {
   }
 
